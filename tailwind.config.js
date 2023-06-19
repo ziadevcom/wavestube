699b388c746a7756448435129dc78284d2325679
= <=>
2023-06-19 18:53:11 +0500

    add custom grid in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "3r": "auto 1fr auto",
+      },
+    },
   },
   plugins: [],
 };
